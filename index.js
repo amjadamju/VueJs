c2049602a745e1289071ac1435eecb5d0c1e41ed
amjadamju <amjadpanjeeri@gmail.com>
2020-03-12 22:43:46 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+         product : 'Amjad',
+         image : 'IMG_0758-01.jpeg' ,
+         description : 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur risus nibh, rutrum eu felis vel, euismod sagittis diam. Mauris dignissim. '
+    }
+  })
